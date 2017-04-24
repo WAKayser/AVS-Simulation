22d3b814348473245285f39732a3adf1a2949e6e
Wouter Kayser <wouterkayser@gmail.com>
2017-04-24 16:56:17 +0200

    Start of noisegen function

--- matlabcode/noisegen.m
+++ matlabcode/noisegen.m
@@ -1,7 +1,18 @@
-function [ signal ] = noisegen(seconds, samplingrate, dB)
+function [ noisesignal ] = noisegen(cleansignal, dB)
 %NOISEGEN Create noise to overlay on the signal
 %   Wrapper for AWGN matlab function
-
-
+%   Can later be extended to create more advanced noise patterns
+%   Clean signal will be a 3 dimensional matrix
+%   The first dimension will be time
+%   The second dimension will be the y and x components
+%   the third dimension will be the index of the AVS
+    
+    avsnum = size(cleansignal, 3);
+    noisesignal = zeros(size(cleansignal));
+    for avs = 1:avsnum
+        for channel = 1:2
+            noisesignal(:,channel,avs) = awgn(cleansignal(:,channel, avs), dB);
+        end
+    end
 end
 
---
+++ matlabcode/test functions/noisegen_test.m
@@ -0,0 +1,9 @@
+function noisegen_test
+%NOISEGEN_TEST Short test function for noisegen function
+
+z = zeros(100,2,3);
+n = noisegen(z, 40);
+
+
+end
+
