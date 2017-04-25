cb664e6b4c3d880ff7ee50b14d2fc9ebe6eae4ac
Wouter Kayser <wouterkayser@gmail.com>
2017-04-25 09:45:51 +0200

    Finished test function for noisegen

--- matlabcode/test functions/noisegen_test.m
+++ matlabcode/test functions/noisegen_test.m
@@ -1,9 +1,14 @@
 function noisegen_test
 %NOISEGEN_TEST Short test function for noisegen function
-
-z = zeros(100,2,3);
-n = noisegen(z, 40);
-
-
+    % It displays all the noise it would add to a signal
+    z = zeros(100,2,3);
+    n = noisegen(z, 40);
+    for a = 1:2
+        for b = 1:3
+            subplot(3,2, (a) + 2*(b-1));
+            plot(n(:,a,b));
+            xlabel(['a: ' num2str(a) ' b:' num2str(b)])
+        end
+    end
 end
 
