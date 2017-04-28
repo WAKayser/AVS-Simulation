74c6ce22562fc20e843b6053327fd3bede0c5d09
Wouter Kayser <wouterkayser@gmail.com>
2017-04-28 11:38:32 +0200

    hiding away the current changes to show_setup

--- matlabcode/helperfunction/show_setup.m
+++ matlabcode/helperfunction/show_setup.m
@@ -1,7 +1,9 @@
-function show_setup
+function show_setup(eventdata, avsdata)
 %SHOW_SETUP This is will be used to create top down view of 
 %   the arrays and the eventlocations
-
-
+    l = eventdata.location(real(l)
+    scatter(real(eventdata.location), imag(eventdata.location));
+    hold on;
+    scatter(real(avsdata.location), imag(avsdata.location));
 end
 
