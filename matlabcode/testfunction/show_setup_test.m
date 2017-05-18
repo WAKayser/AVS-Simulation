e45e5ddeee0e26fd5bc3358793e52bf18f1be0f1
Lennart Noordsij <lennart.noordsij@gmail.com>
2017-05-18 12:13:06 +0200

    show setup afgemaakt

--- matlabcode/testfunction/show_setup_test.m
+++ matlabcode/testfunction/show_setup_test.m
@@ -1,11 +1,11 @@
 function show_setup_test
 %SHOW_SETUP_TEST Summary of this function goes here
 %   Detailed explanation goes here
-    avsdata(:,:,1) = create_array(0+1j, 50, 2, pi, 0);
-    avsdata(:,:,2) = create_array(5000+1j, 50, 2, 0, pi);
+    avsdata(:,:,1) = create_array(0+0j, 50, 2, pi, 0);
+    avsdata(:,:,2) = create_array(5000+300j, 50, 2, 0, pi);
 
-    eventdata(1) = struct('type','pulse','delay',0.25,'duration',0.1,'amplitude',1, 'freq', 0, 'exty', 0,'extfs', 0, 'location', 500+200j);
-    eventdata(2) = struct('type','cosine','delay',0.5,'duration',0.1,'amplitude',1, 'freq', 2000, 'exty', 0,'extfs', 0, 'location', -1000+800j);
+    eventdata(1) = struct('type','pulse','delay',0.25,'duration',0.1,'amplitude',1, 'freq', 0, 'exty', 0,'extfs', 0, 'up', 0, 'down', 0,'location', 1000+1000j);
+    eventdata(2) = struct('type','cosine','delay',0.5,'duration',0.1,'amplitude',1, 'freq', 2000, 'exty', 0,'extfs', 0, 'up', 0, 'down', 0, 'location', 600-800j);
 
     show_setup(eventdata, avsdata);
 end
