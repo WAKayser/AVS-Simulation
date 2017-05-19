c16f08b2daca9018227bcf5b003cee0cd92360e5
Lennart Noordsij <lennart.noordsij@gmail.com>
2017-05-19 11:39:54 +0200

    Change in calculation for the delay, to better fit the data

--- matlabcode/singlefunction/loctransform.m
+++ matlabcode/singlefunction/loctransform.m
@@ -3,7 +3,7 @@ function [Z] = loctransform(transvec, sourcesignal)
 %   transvorm is complex location 
 %   sourcesignal is signal at source
 
-    delay = floor(abs(transvec) / 0.25);
+    delay = floor(abs(transvec) / (343 / 272000));
     loss = abs(transvec) * 1; % simple aproximation of loss
     sigdelay = [zeros(delay, 1); sourcesignal(1:end-delay)] / loss;
     Z = sigdelay(1:17:end) * transvec / abs(transvec);
