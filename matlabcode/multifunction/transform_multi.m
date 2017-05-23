0e445581a9dd5ee313089e49e19dcfaf524a85cf
Wouter Kayser <wouterkayser@gmail.com>
2017-05-23 10:31:14 +0200

    Fixed it now, niet meer zeuren

--- matlabcode/multifunction/transform_multi.m
+++ matlabcode/multifunction/transform_multi.m
@@ -1,14 +1,16 @@
-function Z = transform_multi(eventdata, avsdata, E)
+function [Z, Pz] = transform_multi(eventdata, avsdata, E)
 %TRANSFORM_MULTI combine all events and calculate 
 %   Detailed explanation goes he
 
     Z = zeros(size(E, 1)/17, size(avsdata, 2), size(avsdata, 3));
+    Pz = zeros(size(E, 1)/17, size(avsdata, 2), size(avsdata, 3));
     for i = 1:size(avsdata, 2)
         for k = 1:size(avsdata, 3)
             for j = 1:size(eventdata, 2)
                 transvec = eventdata(1,j).location - avsdata(1,i,k).location;
-                avswave = loctransform(transvec, E(:,j));
+                [avswave, pressure] = loctransform(transvec, E(:,j));
                 Z(:,i,k) = Z(:,i,k) + avswave;
+                Pz(:,i,k) = Pz(:,i,k) + pressure;
             end
         end
     end
