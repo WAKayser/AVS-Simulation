389b69dd1969ef21d47b6497110c97e2f4cbc8ff
Wouter Kayser <wouterkayser@gmail.com>
2017-05-26 15:30:52 +0200

    Expanded smoother, wel smoother, niet beter

--- matlabcode/testfunction/datatomodelsim_test.m
+++ matlabcode/testfunction/datatomodelsim_test.m
@@ -1,12 +1,12 @@
 clear; close all;
 
 avsdata(:,:,1) = create_array(0, 50, 1, pi/2, 0);
-eventdata(1) = struct('type','cosine','delay',0.25,'duration',0.1,'amplitude',5, 'freq', 125, 'exty', 0,'extfs', 0, 'location', 5j);
+eventdata(1) = struct('type','cosine','delay',0.25,'duration',0.1,'amplitude',5, 'freq', 100, 'exty', 0,'extfs', 0, 'location', 5j);
 
 E = eventgen_multi(eventdata, 1);
 [Z, Pz] = transform_multi(eventdata, avsdata, E);
-N = noisegen(Z, 10);
-Pz = noisegen(Pz, 5);
+N = noisegen(Z, 100);
+Pz = noisegen(Pz, 100);
 [P, A] = avsreceiver_multi(N, Pz, avsdata);
 
 subplot(211);
--- verilogcode/smoother.v
+++ verilogcode/smoother.v
@@ -6,20 +6,22 @@ module smoother(clock, reset, datain, dataout);
 	input [bus-1:0] datain;
 	output reg [bus-1:0] dataout;
 
-	reg [bus-1:0] previous, fallback;
+	reg [bus-1:0] previous, second, fallback;
 
 	always @(posedge clock) begin
 		if (reset) begin
 			{previous, fallback, dataout} = 0;
 		end else begin
-			if (previous == datain) begin
+			if (previous == datain & previous == second) begin
 				dataout <= datain;
 				fallback <= datain;
 				previous <= datain;
+				second <= previous;
 			end else begin
 				dataout <= fallback;
 				fallback <= fallback;
 				previous <= datain;
+				second <= previous;
 			end
 		end
 	end
