9724dca9f3fc16b3e1ff6bfbd0a3e2c02ef474be
Wouter Kayser <wouterkayser@gmail.com>
2017-06-08 11:23:42 +0200

    detection plot adjustement

--- matlabcode/helperfunction/detection_plot.m
+++ matlabcode/helperfunction/detection_plot.m
@@ -1,4 +1,4 @@
-function detection_plot(detection, eventVec, peakMatrix, timeStamp, P)
+function detection_plot(detection, eventVec, peakMatrix, P)
     Freq=[];
     Start=[];
     Stop=[];
@@ -15,7 +15,6 @@ function detection_plot(detection, eventVec, peakMatrix, timeStamp, P)
     end
     
     PM=[];
-    TS=[];
     
      for j = 1:size(detection, 3)
         for i = 1:size(detection, 2)
@@ -28,8 +27,8 @@ function detection_plot(detection, eventVec, peakMatrix, timeStamp, P)
             eventVec(eventVec(:,i,j)==0 ,i ,j) = nan;
             plot(tas2,eventVec(:,i,j),'x') 
             PM = cell2mat(peakMatrix(1,i,j));
-            TS = cell2mat(timeStamp(1,i,j));
             subplot(212)
+            TS = (1:size(PM, 1)) ./ Fs;
             plot(TS, PM(1:end,:,1));   
         end
      end
