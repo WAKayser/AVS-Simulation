af118198bad42675b9aefb405fb7c0e43f48e730
Wouter Kayser <wouterkayser@gmail.com>
2017-06-14 14:59:11 +0200

    parameter changes

--- matlabcode/testfunction/compare_test.m
+++ matlabcode/testfunction/compare_test.m
@@ -9,15 +9,15 @@ Fs = 16000;
 avsdata(:,:,1) = create_array(0, 0, 1, pi/2, 0); 
 eventdata(1) = struct('type','cosine','delay',0.3,'duration',0.5,'amplitude', sqrt(2), 'freq',400, 'location', 1);
 eventdata(2) = struct('type','cosine','delay',0.5,'duration',1,'amplitude',sqrt(2), 'freq',4763, 'location', 1);
-eventdata(3) = struct('type','pulse','delay',2,'duration',0.1,'amplitude',sqrt(2), 'freq',2577, 'location', 1);
+eventdata(3) = struct('type','pulse','delay',2,'duration',0.1,'amplitude',2, 'freq',2577, 'location', 1);
 E = eventgen_multi(eventdata, 3);
 %E = E(1:17:end,:,:);
 %E = zeros(Fs*1000, 1);
 
 [Z, Pz] = transform_multi(eventdata, avsdata, E);
-N = noisegen(Z, 20, 2);
+N = noisegen(Z, 14, 2);
 %A=1;
-Pz = noisegen(Pz, 20, 2);
+Pz = noisegen(Pz, 14, 2);
 [P, A] = avsreceiver_multi(N, Pz, avsdata);
 % plot(filter(antinoise, P))
 %show_setup(eventdata,avsdata);
@@ -28,8 +28,8 @@ Pz = noisegen(Pz, 20, 2);
     DSPparam.long = 2000;                       % LTA parameter
     DSPparam.short = 400;                       % STA parameter
     DSPparam.trig = 10;                          % Trigger number
-    DSPparam.stFac = 5;                         % event > threshold * factor
-    DSPparam.endFac = 1;                        % event end < threshold * endFactor
+    DSPparam.stFac = 2;                         % event > threshold * factor
+    DSPparam.endFac = 2;                        % event end < threshold * endFactor
     DSPparam.freqFac = 20;                       % used for detecting peaks
     param.start = DSPparam.short./Fs;         % Error margin on start time
     param.stop = DSPparam.short./Fs;          % Error margin on stop time
